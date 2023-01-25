6e61292394f4cec866eccfa970c3bc7dfc0955ba
Boris <boris.chobyrka@gmail.com>
2023-01-25 20:29:44 +0000

    Initial GraphQL Client & Server boilerplate setup

---
+++ frontend/src/graphql/apollo-client.ts
@@ -0,0 +1,13 @@
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
+  credentials: 'include',
+});
+
+// const webSocketLink = 1;
+
+export const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
